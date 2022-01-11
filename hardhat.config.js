b7306c2d1b93d3d083253ecbd0d8d661fe8c0593
Vladimir Haltakov <vladimir.haltakov@gmail.com>
2022-01-11 14:31:00 +0100

    Add defaults for env variables
    This allows users to run all local commands without creating an .env
    file by setting default values for all env variables.

--- hardhat.config.js
+++ hardhat.config.js
@@ -4,21 +4,23 @@ require("hardhat-gas-reporter");
 require("solidity-coverage");
 require("dotenv").config();
 
-const { MUMBAI_API_URL, POLYGON_API_URL, PRIVATE_KEY, POLYSCAN_KEY } = process.env;
+const MUMBAI_API_URL = process.env.MUMBAI_API_URL || "";
+const POLYGON_API_URL = process.env.POLYGON_API_URL || "";
+const PRIVATE_KEY = process.env.PRIVATE_KEY || "0000000000000000000000000000000000000000000000000000000000000000";
+const POLYSCAN_KEY = process.env.POLYSCAN_KEY || "";
 
 module.exports = {
     solidity: "0.8.2",
     networks: {
         hardhat: {},
-        // Uncomment according to the network you wanna use
-        // mumbai: {
-        //     url: MUMBAI_API_URL,
-        //     accounts: [`0x${PRIVATE_KEY}`],
-        // },
-        // polygon: {
-        //     url: POLYGON_API_URL,
-        //     accounts: [`0x${PRIVATE_KEY}`],
-        // },
+        mumbai: {
+            url: MUMBAI_API_URL,
+            accounts: [`0x${PRIVATE_KEY}`],
+        },
+        polygon: {
+            url: POLYGON_API_URL,
+            accounts: [`0x${PRIVATE_KEY}`],
+        },
     },
     etherscan: {
         apiKey: POLYSCAN_KEY,
