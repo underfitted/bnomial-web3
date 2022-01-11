51a7803fb6ece06bc80e81d747639d249d173f0b
Vladimir Haltakov <vladimir.haltakov@gmail.com>
2022-01-11 14:46:19 +0100

    Add .prettierrc and reformat the codebase

--- scripts/deployNFT.js
+++ scripts/deployNFT.js
@@ -1,17 +1,17 @@
 async function main() {
-    const [deployer] = await ethers.getSigners();
+    const [deployer] = await ethers.getSigners()
 
-    console.log("Deploying contracts with the account:", deployer.address);
-    console.log("Account balance:", (await deployer.getBalance()).toString());
+    console.log("Deploying contracts with the account:", deployer.address)
+    console.log("Account balance:", (await deployer.getBalance()).toString())
 
-    const BnomialNFTContract = await ethers.getContractFactory("BnomialNFT");
-    const NFTcontract = await BnomialNFTContract.deploy();
-    console.log("NFTcontract Contract address:", NFTcontract.address);
+    const BnomialNFTContract = await ethers.getContractFactory("BnomialNFT")
+    const NFTcontract = await BnomialNFTContract.deploy()
+    console.log("NFTcontract Contract address:", NFTcontract.address)
 }
 
 main()
     .then(() => process.exit(0))
     .catch((error) => {
-        console.error(error);
-        process.exit(1);
-    });
+        console.error(error)
+        process.exit(1)
+    })
--- test/BnomialToken.test.js
+++ test/BnomialToken.test.js
@@ -1,94 +1,86 @@
-const { BigNumber } = require("@ethersproject/bignumber");
-const { expect } = require("chai");
-const { ethers } = require("hardhat");
-
+const { BigNumber } = require("@ethersproject/bignumber")
+const { expect } = require("chai")
+const { ethers } = require("hardhat")
 
 describe("BnomialToken", () => {
-    let INITIAL_SUPPLY =  BigNumber.from(0);
-    const NAME = "Bnomial Token";
-    const SYMBOL = "BNO";
-    
-    let contract;
-    let owner;
-    let addr1;
+    let INITIAL_SUPPLY = BigNumber.from(0)
+    const NAME = "Bnomial Token"
+    const SYMBOL = "BNO"
+
+    let contract
+    let owner
+    let addr1
 
     beforeEach(async function () {
-        BnomialTokenContract = await ethers.getContractFactory("BnomialToken");
-        [owner, addr1] = await ethers.getSigners();     
-        contract = await BnomialTokenContract.deploy();
-    });
+        BnomialTokenContract = await ethers.getContractFactory("BnomialToken")
+        ;[owner, addr1] = await ethers.getSigners()
+        contract = await BnomialTokenContract.deploy()
+    })
 
-    it('has a name', async function () {
+    it("has a name", async function () {
         console.log(contract.name())
-        expect(await contract.name()).to.be.equal(NAME);
-    });
-    
-    it('has a symbol', async function () {
-    expect(await contract.symbol()).to.be.equal(SYMBOL);
-    });
+        expect(await contract.name()).to.be.equal(NAME)
+    })
 
-    it('assigns the initial total supply to the creator', async function () {
-    balance_owner = await contract.balanceOf(owner.address);    
-    expect(balance_owner).to.equal(INITIAL_SUPPLY.toNumber());
-    });
+    it("has a symbol", async function () {
+        expect(await contract.symbol()).to.be.equal(SYMBOL)
+    })
 
-    it("should mint a token", async () => {     
-        await contract.mint(addr1.address, 1);
-        total_supply = await contract.totalSupply();
-        initial_supply = ( INITIAL_SUPPLY.toNumber())+1;        
-        expect(total_supply).to.equal(initial_supply);
-        expect(await contract.balanceOf(addr1.address)).to.equal(1);
-     
-    });
+    it("assigns the initial total supply to the creator", async function () {
+        balance_owner = await contract.balanceOf(owner.address)
+        expect(balance_owner).to.equal(INITIAL_SUPPLY.toNumber())
+    })
+
+    it("should mint a token", async () => {
+        await contract.mint(addr1.address, 1)
+        total_supply = await contract.totalSupply()
+        initial_supply = INITIAL_SUPPLY.toNumber() + 1
+        expect(total_supply).to.equal(initial_supply)
+        expect(await contract.balanceOf(addr1.address)).to.equal(1)
+    })
 
     it("should allow only owner to mint a token", async () => {
         // Expect minting from another wallet to fail
         await expect(contract.connect(addr1).mint(addr1.address, 1)).to.be.revertedWith(
             "VM Exception while processing transaction: reverted with reason string 'Ownable: caller is not the owner'"
-        );
+        )
         // Expect minting from owner to succeed
-        await contract.mint(addr1.address, 1);
-    });
+        await contract.mint(addr1.address, 1)
+    })
 
-   
     it("should pause the contract", async () => {
         // Pause the minting
-        await contract.connect(owner).pause();        
+        await contract.connect(owner).pause()
 
         // await expect( await contract.mint(addr1.address, 1) ).to.be.reverted;
         // Expect minting to fail
-        await expect(  contract.mint(addr1.address, 1) ).to.be.revertedWith(
+        await expect(contract.mint(addr1.address, 1)).to.be.revertedWith(
             "VM Exception while processing transaction: reverted with reason string 'Pausable: paused'"
-        );
-    });
+        )
+    })
 
     it("should unpause the contract", async () => {
         // Pause and unpause the contract
-        await contract.connect(owner).pause();
-        await contract.connect(owner).unpause();
+        await contract.connect(owner).pause()
+        await contract.connect(owner).unpause()
 
         // Minting should work
-        await contract.mint(addr1.address, 1);
-        expect(await contract.paused()).to.equal(false);
-    });
+        await contract.mint(addr1.address, 1)
+        expect(await contract.paused()).to.equal(false)
+    })
 
     it("should reduce balance after burn", async () => {
         // Pause and unpause the contract
         balance_before_mint = await contract.balanceOf(owner.address)
-        
+
         // console.log('balance_before_mint ' + JSON.stringify(balance_before_mint.toNumber()))
-        await contract.mint(owner.address, 1);
+        await contract.mint(owner.address, 1)
         balance_after_mint = await contract.balanceOf(owner.address)
         // console.log('balance_after_mint: ' + JSON.stringify(balance_after_mint.toNumber()))
 
-        await contract.burn( 1);
+        await contract.burn(1)
         balance_after_burn = await contract.balanceOf(owner.address)
         // console.log('balance_after_burn: ' + JSON.stringify(balance_after_burn.toNumber()))
-        expect(balance_before_mint).to.equal(balance_after_burn);
-        
-    });
-
-    
-
-
-});
+        expect(balance_before_mint).to.equal(balance_after_burn)
+    })
+})
