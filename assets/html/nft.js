b70a1b49d68c3f23c014f676685c13aabcf81e59
Vladimir Haltakov <vladimir.haltakov@gmail.com>
2022-01-19 21:53:09 +0100

    Remove obsolete HTML files

--- assets/html/nft.js
+++
@@ -1,8 +0,0 @@
-function init() {
-    const urlParams = new URLSearchParams(window.location.search)
-    const badges = urlParams.get("badges")
-
-    document.getElementById("badges").innerHTML = badges
-}
-
-window.addEventListener("load", init)
